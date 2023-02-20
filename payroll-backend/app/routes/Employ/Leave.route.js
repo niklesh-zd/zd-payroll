15631b35bc2a2c02994b3160aeb47556f236f4e6
abhishekraj-zd <Abhishek.raj@zecdata.com>
2023-02-20 17:14:44 +0530

    today and yesterday attandance

--- payroll-backend/app/routes/Employ/Leave.route.js
+++ payroll-backend/app/routes/Employ/Leave.route.js
@@ -2,7 +2,7 @@
 "use strict"
 
 const router = require("express").Router()
-const { Leave, get_leave, leave_delete, update_laeve ,get_User_leave,get_user_leave_id,get_User_leave_count,get_day_leave} = require('../../controllers/Employ/Leave.cotroller')
+const { Leave, get_leave, leave_delete, update_laeve ,get_User_leave,get_user_leave_id,get_User_leave_count,get_today_leave, get_yesterday_leave} = require('../../controllers/Employ/Leave.cotroller')
 
 
 router.post('/leave', Leave) // to save leave in to db
@@ -13,7 +13,8 @@ router.post('/leave_dalete/:id', leave_delete) // to delete any leave
 router.post('/leave_update', update_laeve) // to update any leave
 router.post('/get_User_leave', get_User_leave) // get leave of a user between date range
 router.post('/get_User_leave_count', get_User_leave_count) // for a user in date range
-router.get('/get_day_leave', get_day_leave) // present and absent employees on current day
+router.get('/get_today_leave', get_today_leave) // present and absent employees on today
+router.get('/get_yesterday_leave', get_yesterday_leave) // present and absent employees on yesterday
 
 
 module.exports = router;
