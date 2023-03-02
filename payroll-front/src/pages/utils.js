b0119d75fb8a7a756e12184026c4ce4d13f445a1
atul.asati <atul.asati@aurodatateam>
2023-03-02 19:06:09 +0530

    update utils again

--- payroll-front/src/pages/utils.js
+++ payroll-front/src/pages/utils.js
@@ -1,4 +1,4 @@
-var host = "https://lazy-teal-cormorant-robe.cyclic.app"
+var host = ""
 // var host = "http://192.168.29.186:7071"  faizan
 // var host = "http://192.168.29.147:7071"  //Sanjay
 export default host
