1173f4157cf1ee570cc224859958f786695c5774
atul.asati <atul.asati@aurodatateam>
2023-03-03 12:40:12 +0530

    login auth added

--- payroll-front/src/App.js
+++ payroll-front/src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import SideBar from "./components/Sidebar/SideBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import Salary from "./pages/Salary";
 import Dashboard from "./pages/Dashboard";
-import Logout from "./pages/logout";
 import AddEmployee from "./pages/Employee/AddEmployee";
 import ManageEmpyee from "./pages/Employee/manageEmpyee";
 import EmpEdit from "./pages/Employee/EmpEdit";
@@ -13,27 +12,35 @@ import Leaves from "./pages/Leaves/Leave";
 import LeaveDetails from "./pages/Leaves/LeaveDetails";
 import UserLeaveDetails from "./pages/Leaves/UserLeaveDetails";
 import Downloadslip from "./pages/Salary_slip/downloadslip";
+import LoginPage from "./Auth/LoginPage";
+import { useState } from "react";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
   return (
     <Router>
-      <SideBar>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/settings" element={<Logout />} />
-          <Route path="/settings/salary:id" element={<Salary/>} />
-          <Route path="/settings/profile" element={<AddEmployee/>} />
-          <Route path="/settings/manageprofile" element={<ManageEmpyee/>} />
-          <Route path="/settings/EmpEdit:id" element={<EmpEdit />} />
-          <Route path="/settings/EmpDetail:id" element={<EmpDetail/>} />
-          <Route path="/settings/leave" element={<Leaves />} />
-          <Route path="/settings/leavedetails" element={<LeaveDetails />} />
-          <Route path="/settings/userleavedetails:id" element={<UserLeaveDetails />} />
-          <Route path="/download:id" element={<Downloadslip />} />
-         
-          
-        </Routes>
-      </SideBar>
+      {!loggedIn ? (
+        <LoginPage onLogin={handleLogin} />
+      ) : (
+        <SideBar>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/settings/salary/:id" element={<Salary />} />
+            <Route path="/settings/profile" element={<AddEmployee />} />
+            <Route path="/settings/manageprofile" element={<ManageEmpyee />} />
+            <Route path="/settings/EmpEdit/:id" element={<EmpEdit />} />
+            <Route path="/settings/EmpDetail/:id" element={<EmpDetail />} />
+            <Route path="/settings/leave" element={<Leaves />} />
+            <Route path="/settings/leavedetails" element={<LeaveDetails />} />
+            <Route path="/settings/userleavedetails/:id" element={<UserLeaveDetails />} />
+            <Route path="/download/:id" element={<Downloadslip />} />
+          </Routes>
+        </SideBar>
+      )}
     </Router>
   );
 }
