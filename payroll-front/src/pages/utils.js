659e8e6767032671b10788e58e323f78d6848f55
atul.asati <atul.asati@aurodatateam>
2023-03-16 19:36:57 +0530

    build update

--- payroll-front/src/pages/utils.js
+++ payroll-front/src/pages/utils.js
@@ -1,8 +1,7 @@
 
 //console.log(process.env.HOST);
-// var host = "" 
+var host = "" 
 // var host = "http://localhost:7071"
-var host = "144.126.254.221:7071"
 // var host = "http://192.168.29.147:7071"
 //var host = `${process.env.HOST}:${process.env.PORT}`;  //Sanjay
 export default host
