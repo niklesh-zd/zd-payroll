87a1ad6007a386c655962c39e2b14ccb34069c01
atul.asati <atul.asati@aurodatateam>
2023-03-18 16:12:29 +0530

    logout confimation

--- payroll-front/src/Auth/Logout.js
+++ payroll-front/src/Auth/Logout.js
@@ -1,60 +1,37 @@
-
-import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Button } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 export default function Logout() {
-    const navigate = useNavigate();
-    const [showLogoutDialog, setShowLogoutDialog] = useState(false);
-
-    function handleLogout() {
-        setShowLogoutDialog(true);
-
-        // localStorage.removeItem('token');
-        // navigate('/');
-    }
-    function handleLogoutConfirm() {
-        // Perform the logout action here...
-        localStorage.removeItem('token');
-        navigate('/');
+  const navigate = useNavigate();
+  function handleLogout() {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "Are you realy want to logout!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      cancelButtonText: "No",
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem("token");
+        navigate("/");
         window.location.reload();
-        setShowLogoutDialog(false);
-    }
-
-    function handleLogoutCancel() {
-        setShowLogoutDialog(false);
-    }
-
-    function LogoutDialog(props) {
-        return (
-            <div  >
-                <p>Do you really want to log out?</p>
-
-                <button onClick={props.onConfirm}>Yes</button>
-                <button onClick={props.onCancel}>No</button>
-            </div>
-        );
-    }
-    return (
-        // <div style={{
-        //     paddingLeft: "1100px",
-        //     marginTop: "10px"
-        // }}>
-        //     <button onClick={handleLogout}>Logout</button>
-        // </div>
-        <div style={{
-            paddingLeft: "1100px",
-            marginTop: "25px"
-        }}>
-            <button onClick={handleLogout}>Log out</button>
-            {showLogoutDialog && (
-                <LogoutDialog
-                    onConfirm={handleLogoutConfirm}
-                    onCancel={handleLogoutCancel}
-                />
-            )}
-        </div>
-
-    );
+      }else{
+        navigate("/");
+      }
+    });
+  }
+  useEffect(() => {
+    handleLogout();
+  }, []);
+  return (
+    // <div style={{ float: "right" }}>
+    //   <Button onClick={handleLogout}>Log out</Button>
+    // </div>
+    <></>
+  );
 }
-
-
