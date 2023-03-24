26ed5f0087ca90e911c8c4947e5ec5a35fce2223
niklesh-zd <108116835+niklesh-zd@users.noreply.github.com>
2023-03-24 13:06:42 +0530

    Update mongooseConnecter.util.js

--- app/utils/mongooseConnecter.util.js
+++ app/utils/mongooseConnecter.util.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { connect, connection } = require("mongoose");
+const { connect, connection, mongoose} = require("mongoose");
 require('dotenv').config();
 // const url =`mongodb+srv://${process.env.mongo_pass}:${process.env.mongo_pass}@cluster0.9bxysok.mongodb.net/?retryWrites=true&w=majority`
 const url = `mongodb+srv://payroll:payroll123@cluster0.8pzycmy.mongodb.net/?retryWrites=true&w=majority`
